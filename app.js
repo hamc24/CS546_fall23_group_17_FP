af71145bfd3bb06b324a252305441eb6a5283282
RudolphSedlin <Rudolph.Sedlin@protonmail.com>
2023-11-12 19:35:05 -0500

    Added handlebars to the app and a CSS directory, blank at the moment.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 // Set up an express server that we will use to recieve client requests.
 import express from 'express';
 const app = express();
-app.use(express.json());
-import configRoutesFunction from './routes/index.js';
+import configRoutes from './routes/index.js';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+import exphbs from 'express-handlebars';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-configRoutesFunction(app);
+const staticDir = express.static(__dirname + '/public');
+
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+  // If the user posts to the server with a property called _method, rewrite the request's method
+  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+  // rewritten in this middleware to a PUT route
+  if (req.body && req.body._method) {
+    req.method = req.body._method;
+    delete req.body._method;
+  }
+
+  // let the next middleware run:
+  next();
+};
+
+app.use('/public', staticDir); // A CSS directory as shown in lecture 8.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(rewriteUnsupportedBrowserMethods);
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
